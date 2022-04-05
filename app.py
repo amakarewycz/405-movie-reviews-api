9cd9b48ac863e0c5f4a7f46edc6c40b977c8e453
Alex Makarewycz <alexander_makarewycz@intuit.com>
2022-04-05 07:12:11 +0000

    tyring to get dynamic styling

--- app.py
+++ app.py
@@ -5,15 +5,41 @@ from dash.exceptions import PreventUpdate
 from helpers.key_finder import api_key
 from helpers.api_call import *
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-
+from enum import Enum
 
 ########### Define a few variables ######
-
 tabtitle = 'Movies'
 sourceurl = 'https://www.kaggle.com/tmdb/tmdb-movie-metadata'
 sourceurl2 = 'https://developers.themoviedb.org/3/getting-started/introduction'
 githublink = 'https://github.com/amakarewycz/405-movie-reviews'
+
+class RatingStyle(Enum):
+    Negative = { 'padding': '12px',
+                    'font-size': '22px',
+                    # 'height': '400px',
+                    'border': "#FF7A33",
+                    'color': 'rgb(255, 255, 255)',
+                    'backgroundColor': '#536869',
+                    'textAlign': 'left',
+                    }
+    Positive = { 'padding': '12px',
+                    'font-size': '22px',
+                    # 'height': '400px',
+                    'border': "#64FF33",
+                    'color': 'rgb(255, 255, 255)',
+                    'backgroundColor': '#536869',
+                    'textAlign': 'left',
+                    }
+    Neutral = { 'padding': '12px',
+                    'font-size': '22px',
+                    # 'height': '400px',
+                    'border': "#DCDEB3",
+                    'color': 'rgb(255, 255, 255)',
+                    'backgroundColor': '#536869',
+                    'textAlign': 'left',
+                    }
+
+    
 
 ####### Write your primary function here
 def sentiment_scores(sentence):
@@ -40,7 +66,6 @@ def sentiment_scores(sentence):
     return response1, response2, response3, response4, response5
 
 
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -55,9 +80,8 @@ app.layout = html.Div(children=[
     html.Div([
         html.H1(['Movie Reviews']),
         html.Div([
-            html.Div([
-                html.Div('Randomly select a movie summary'),
-                html.Button(id='eek-button', n_clicks=0, children='API call', style={'color': 'rgb(255, 255, 255)'}),
+            html.Div(id="movie-container",children=[
+                html.Button(id='eek-button', n_clicks=0, children='Click to randomly select a Movie', style={'color': 'rgb(255, 255, 255)'}),
                 html.Div(id='movie-title', children=[]),
                 html.Div(id='movie-release', children=[]),
                 html.Div(id='movie-overview', children=[]),
@@ -72,25 +96,24 @@ app.layout = html.Div(children=[
             className='twelve columns'),
         ], className='twelve columns'),
         html.Br(),
-
     ], className='twelve columns'),
         # Output
     html.Div([
         # Footer
                   html.Div([
-                html.H2('Sentiment:'),
+                html.H3('Sentiment of movie overview:'),
                 html.Table([
-                                      html.Thead([
-                                      html.Tr([
-                     html.Td(html.P("Negative / %")),
+                   html.Thead([
+                    html.Tr([
+                     html.Td(html.Div("Negative / %")),
 
-                     html.Td(html.P("Neutral / %")),
+                     html.Td(html.Div("Neutral / %")),
 
-                     html.Td(html.P("Positive / %")),
-                     html.Td(html.P("Overall Sentiment"))
+                     html.Td(html.Div("Positive / %")),
+                     html.Td(html.Div("Overall Sentiment"))
                                       ]),
                                       ]),
-                                                        html.Tbody([  
+                 html.Tbody([  
                   html.Tr([
                      html.Td(html.Div(id='output-div-2')),
                      html.Td(html.Div(id='output-div-3')),
@@ -100,12 +123,12 @@ app.layout = html.Div(children=[
                              ]),
                     ]),
             ], style={ 'padding': '12px',
-                    'font-size': '14px',
+                    'font-size': '22px',
                     # 'height': '400px',
                     'border': 'thick blue solid',
                     'color': 'rgb(255, 255, 255)',
                     'backgroundColor': '#234553',
-                    'textAlign': 'right',
+                    'textAlign': 'left',
                     },
             className='twelve columns'),  
         html.Br(),
@@ -140,6 +163,7 @@ def on_click(n_clicks, data):
                 Output(component_id='output-div-3', component_property='children'),
                 Output(component_id='output-div-4', component_property='children'),
                 Output(component_id='output-div-5', component_property='children'),
+                Output(component_id='movie-container', component_property='style')
                 ],
               [Input('tmdb-store', 'modified_timestamp')],
               [State('tmdb-store', 'data')])
@@ -149,12 +173,11 @@ def on_data(ts, data):
     else:
         message = sentiment_scores(data['overview'])
         return data['title'], data['release_date'], data['overview'], \
-               message[1],message[2],message[3],message[4]
+               message[1],message[2],message[3],message[4],RatingStyle[message[4]].value
 
-
-   
     
 
 ############ Deploy
 if __name__ == '__main__':
+    #print(RatingStyle["Neutral"].value)
     app.run_server(debug=True)
