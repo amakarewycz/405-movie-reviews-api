158c63938bccc40ac6109ba8b0ddcc4dcbdb6992
Alex Makarewycz <alexander_makarewycz@intuit.com>
2022-04-05 06:31:33 +0000

    working app

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 from helpers.key_finder import api_key
 from helpers.api_call import *
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 
 ########### Define a few variables ######
@@ -11,7 +13,31 @@ from helpers.api_call import *
 tabtitle = 'Movies'
 sourceurl = 'https://www.kaggle.com/tmdb/tmdb-movie-metadata'
 sourceurl2 = 'https://developers.themoviedb.org/3/getting-started/introduction'
-githublink = 'https://github.com/austinlasseter/tmdb-rf-classifier'
+githublink = 'https://github.com/amakarewycz/405-movie-reviews'
+
+####### Write your primary function here
+def sentiment_scores(sentence):
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        final="Positive"
+    elif sentiment_dict['compound'] <= - 0.05 :
+        final="Negative"
+    else :
+        final="Neutral"
+    # responses
+    response1=f"Overall sentiment dictionary is : {sentiment_dict}"
+    response2=f"{round(sentiment_dict['neg']*100, 2)}"
+    response3=f"{round(sentiment_dict['neu']*100, 2)}"
+    response4=f"{round(sentiment_dict['pos']*100,2 )}"
+    response5=f"{final}"
+    return response1, response2, response3, response4, response5
 
 
 
@@ -35,7 +61,6 @@ app.layout = html.Div(children=[
                 html.Div(id='movie-title', children=[]),
                 html.Div(id='movie-release', children=[]),
                 html.Div(id='movie-overview', children=[]),
-
             ], style={ 'padding': '12px',
                     'font-size': '22px',
                     # 'height': '400px',
@@ -44,17 +69,45 @@ app.layout = html.Div(children=[
                     'backgroundColor': '#536869',
                     'textAlign': 'left',
                     },
-            className='six columns'),
-
+            className='twelve columns'),
         ], className='twelve columns'),
         html.Br(),
 
     ], className='twelve columns'),
-
-
         # Output
     html.Div([
         # Footer
+                  html.Div([
+                html.H2('Sentiment:'),
+                html.Table([
+                                      html.Thead([
+                                      html.Tr([
+                     html.Td(html.P("Negative / %")),
+
+                     html.Td(html.P("Neutral / %")),
+
+                     html.Td(html.P("Positive / %")),
+                     html.Td(html.P("Overall Sentiment"))
+                                      ]),
+                                      ]),
+                                                        html.Tbody([  
+                  html.Tr([
+                     html.Td(html.Div(id='output-div-2')),
+                     html.Td(html.Div(id='output-div-3')),
+                     html.Td(html.Div(id='output-div-4')),
+                     html.Td(html.Div(id='output-div-5'))
+                             ]),
+                             ]),
+                    ]),
+            ], style={ 'padding': '12px',
+                    'font-size': '14px',
+                    # 'height': '400px',
+                    'border': 'thick blue solid',
+                    'color': 'rgb(255, 255, 255)',
+                    'backgroundColor': '#234553',
+                    'textAlign': 'right',
+                    },
+            className='twelve columns'),  
         html.Br(),
         html.A('Code on Github', href=githublink, target="_blank"),
         html.Br(),
@@ -62,9 +115,6 @@ app.layout = html.Div(children=[
         html.Br(),
         html.A("Data Source: TMDB", href=sourceurl2, target="_blank"),
     ], className='twelve columns'),
-
-
-
     ]
 )
 
@@ -86,6 +136,10 @@ def on_click(n_clicks, data):
 @app.callback([Output('movie-title', 'children'),
                 Output('movie-release', 'children'),
                 Output('movie-overview', 'children'),
+                Output(component_id='output-div-2', component_property='children'),
+                Output(component_id='output-div-3', component_property='children'),
+                Output(component_id='output-div-4', component_property='children'),
+                Output(component_id='output-div-5', component_property='children'),
                 ],
               [Input('tmdb-store', 'modified_timestamp')],
               [State('tmdb-store', 'data')])
@@ -93,8 +147,13 @@ def on_data(ts, data):
     if ts is None:
         raise PreventUpdate
     else:
-        return data['title'], data['release_date'], data['overview']
+        message = sentiment_scores(data['overview'])
+        return data['title'], data['release_date'], data['overview'], \
+               message[1],message[2],message[3],message[4]
 
+
+   
+    
 
 ############ Deploy
 if __name__ == '__main__':
